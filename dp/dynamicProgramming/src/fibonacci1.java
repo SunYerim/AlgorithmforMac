ce27e06a2de4b8df52193e1d063b9894e3573cce
SunYerim <syl1602@naver.com>
2023-08-21 20:19:55 +0900

    fibonacci1

---
+++ dp/dynamicProgramming/src/fibonacci1.java
@@ -0,0 +1,11 @@
+public class fibonacci1 {
+    public static int fibo(int x){
+        if (x == 1 || x == 2) {
+            return 1;
+        }
+        return fibo(x-1) + fibo(x-2);
+    }
+    public static void main(String[] args) {
+        System.out.println(fibo(4));
+    }
+}

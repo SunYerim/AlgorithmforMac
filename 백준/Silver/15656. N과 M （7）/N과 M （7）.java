0d2f78a80ac1166e46d9ecab506a6f43789907c0
Yerim <101817171+SunYerim@users.noreply.github.com>
2024-02-24 16:18:44 +0900

    [Silver III] Title: N과 M (7), Time: 640 ms, Memory: 123168 KB -BaekjoonHub

---
+++ 백준/Silver/15656. N과 M （7）/N과 M （7）.java
@@ -0,0 +1,47 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int n, m;
+    static int[] numbers;
+    static int[] pickList;
+    static StringBuilder sb = new StringBuilder();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        numbers = new int[n+1];
+        pickList = new int[m];
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            numbers[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(numbers);
+
+        permutation(0);
+        System.out.println(sb);
+    }
+
+    private static void permutation(int cnt) {
+        // 기저
+        if (cnt == m) {
+            for (int i = 0; i < m; i++) {
+                sb.append(pickList[i]).append(" ");
+            }
+            sb.append('\n');
+            return;
+        }
+        // 유도
+        for (int i = 1; i <= n; i++) {
+            pickList[cnt] = numbers[i];
+            permutation(cnt+1);
+        }
+    }
+}

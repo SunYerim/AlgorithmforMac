be772525e7f1edc27ce1f36b11a69d76bc72c28d
Yerim <101817171+SunYerim@users.noreply.github.com>
2024-02-24 18:45:11 +0900

    [Silver II] Title: N과 M (12), Time: 140 ms, Memory: 14500 KB -BaekjoonHub

---
+++ 백준/Silver/15666. N과 M （12）/N과 M （12）.java
@@ -0,0 +1,51 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int n, m;
+    static int[] numbers, pickList;
+    static StringBuilder sb = new StringBuilder();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+
+        numbers = new int[n+1];
+        pickList = new int[m];
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 1; i <= n; i++) {
+            numbers[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(numbers);
+
+        permutation(0, 1);
+        System.out.print(sb);
+    }
+
+    private static void permutation(int cnt, int start) {
+        // 기저
+        if (cnt == m) {
+            for (int i = 0; i < m; i++) {
+                sb.append(pickList[i]).append(" ");
+            }
+            sb.append('\n');
+            return;
+        }
+        // 유도
+        int beforeValue = -1;
+        for (int i = start; i <= n; i++) {
+            if (beforeValue != numbers[i]) {
+                pickList[cnt] = numbers[i];
+                permutation(cnt+1, i);
+                beforeValue = numbers[i];
+            }
+
+        }
+    }
+}

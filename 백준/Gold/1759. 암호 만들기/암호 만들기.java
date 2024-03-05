201971a6fb788baa64c21b9cdc29b7c01ece5774
Yerim <101817171+SunYerim@users.noreply.github.com>
2024-03-06 00:10:27 +0900

    [Gold V] Title: 암호 만들기, Time: 120 ms, Memory: 14832 KB -BaekjoonHub

--- 백준/Gold/1759. 암호 만들기/암호 만들기.java
+++ 백준/Gold/1759. 암호 만들기/암호 만들기.java
@@ -1,81 +1,82 @@
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.StringTokenizer;
 
+/* logic
+    1. L개의 소문자들 중에서 최소 한 개의 모음 & 최소 두 개의 자음
+        => 전체 경우에서 모음 하나도 없는거 재낌.
+        => or 조합 만들고 boolean메서드 선언해서 거르기.*/
 public class Main {
-    static int L,C; // 받아오는 변수
-
-    static char[] code;
-    static char[] alpha; // 문자 배열
-    static ArrayList<String> password; // password 조합으로 될 수 있는 연결 리스트 (크기가 정해지지 않음.)
+    static char[] selected;
+    static char[] alpha;
+    static int l, c;
+    static boolean[] visited;
+    static char[] moeum = new char[]{'a','e','i','o','u'};
+    static StringBuilder sb = new StringBuilder();
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        
         StringTokenizer st = new StringTokenizer(br.readLine());
-
-        L = Integer.parseInt(st.nextToken());
-        C = Integer.parseInt(st.nextToken());
+        l = Integer.parseInt(st.nextToken());
+        c = Integer.parseInt(st.nextToken());
+        selected = new char[l];
+        alpha = new char[c];
+        visited = new boolean[c];
 
         st = new StringTokenizer(br.readLine());
-
-        alpha = new char[C];
-        code = new char[L];
-        for (int i = 0; i < C; i++) {
+        for (int i = 0; i < c; i++) {
             alpha[i] = st.nextToken().charAt(0);
         }
 
-        Arrays.sort(alpha); // 입력받은 글자들을 모두 정렬시킨다.
+        Arrays.sort(alpha);
 
-        // 여기서 조합으로 나올 수 있는 모든 경우의 수 구한 다음  -> 모음 없는 건 delete처리 시켜서 return
+        dfs(0, 0);
 
-        makePassword(0, 0);
-
+        System.out.println(sb);
     }
 
-    // 가능한 password 만드는 메소드
-    public static void makePassword(int x, int idx) {
-        //
-        if (idx == L) {
-            if (isValid()) {
-                System.out.println(code);
+
+    // 조합
+    private static void dfs(int depth, int cnt) {
+        // 기저
+        if (depth == l) {
+            if (isValid(selected)) {
+                for (int i = 0; i < selected.length; i++) {
+                    sb.append(selected[i]);
+                }
+                sb.append("\n");
             }
             return;
         }
 
-        // 아직 길이 L의 코드 길이를 만들지 않았을때,
-        for (int i = x; i < C; i++) {
-            code[idx] = alpha[i];
-            makePassword(i+1, idx+1);
+        // 뽑는다.
+        for (int i = cnt; i < c; i++) {
+            selected[depth] = alpha[i];
+            dfs(depth+1, i+1);
         }
 
     }
 
-
-    // password가 최소 한 개의 모음과 두 개의 자음으로 구성된 게 맞는지 판별하는 메소드
-    public static boolean isValid() {
-        int moeum = 0;
-        int jaeum = 0;
-
-        // a,e,i,o,u
-        for (char c : code) {
-            if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u') {
-                moeum++;
-            } else {
-                jaeum++;
+    private static boolean isValid(char[] tmp) {
+        int mo = 0;
+        int ja = 0;
+        for (int i = 0; i < tmp.length; i++) {
+            boolean isMoeum = false;
+            for (int j = 0; j < 5; j++) {
+                if (tmp[i] == moeum[j]) {
+                    isMoeum = true;
+                    break;
+                }
             }
+            if (isMoeum) mo++;
+            else ja++;
         }
 
-        if (moeum >= 1 && jaeum >= 2) {
+        if (mo >= 1 && ja >= 2)
             return true;
-        } else {
-            return false;
-        }
+        // 충족 안 되면
+        else return false;
     }
-
 
 }

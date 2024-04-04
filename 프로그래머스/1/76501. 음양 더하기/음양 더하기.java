b959a4624fd1dc87baaf141c45e8e548cfdd8db7
Yerim <101817171+SunYerim@users.noreply.github.com>
2024-04-05 00:12:36 +0900

    [level 1] Title: 음양 더하기, Time: 0.06 ms, Memory: 78.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/76501. 음양 더하기/음양 더하기.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int solution(int[] absolutes, boolean[] signs) {
+        int answer = 0;
+        for (int i = 0; i < absolutes.length; i++) {
+            if (signs[i] == true) {
+                answer += absolutes[i];
+            } else if (signs[i] == false) {
+                answer += (absolutes[i] * -1);
+            }
+        }
+        return answer;
+    }
+}

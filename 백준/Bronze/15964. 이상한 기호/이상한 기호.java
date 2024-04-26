10976a570858d3e5eac18e41fff92ec31e794a9d
Yerim <101817171+SunYerim@users.noreply.github.com>
2024-04-27 00:31:50 +0900

    [Bronze V] Title: 이상한 기호, Time: 120 ms, Memory: 14196 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Bronze/15964. 이상한 기호/이상한 기호.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        long a = Long.parseLong(st.nextToken());
+        long b = Long.parseLong(st.nextToken());
+
+        long ans = 0;
+        long n1 = a+b;
+        long n2 = a-b;
+        ans = n1 * n2;
+        System.out.println(ans);
+    }
+}

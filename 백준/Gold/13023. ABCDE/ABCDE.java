e42a422ef9f7052029359c65f55dc5e1bcf592e1
Yerim <101817171+SunYerim@users.noreply.github.com>
2025-01-17 18:53:02 +0900

    [Gold V] Title: ABCDE, Time: 240 ms, Memory: 25928 KB -BaekjoonHub

--- 백준/Gold/13023. ABCDE/ABCDE.java
+++ 백준/Gold/13023. ABCDE/ABCDE.java
@@ -1,20 +1,36 @@
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.StringTokenizer;
 
-class newNode {
-    ArrayList<newNode> friends = new ArrayList<>();
-    boolean visited = false;
-    int index;
-}
-
+/* logic
+    * - arraylist로 친구관계 저장
+    * - 친구 방문 안 했으면 dfs
+    * - depth가 4가 충족됐을때, 기저
+    * - boolean 변수
+    * */
 public class Main {
     static int N, M;
-    static newNode[] graph;
+    static boolean result = false;
+    static boolean[] visited;
+    static Friend[] friends;
 
+    static class Friend {
+        ArrayList<Integer> relationship; // 친구관계
+        int num;
+        boolean isVisited;
+
+        public Friend() {
+
+        }
+
+        public Friend(ArrayList<Integer> relationship, int num, boolean isVisited) {
+            this.relationship = relationship;
+            this.num = num;
+            this.isVisited = isVisited;
+        }
+    }
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
@@ -22,48 +38,60 @@ public class Main {
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
 
-        graph = new newNode[N];
+        friends = new Friend[N];
+
+        // 초기화
         for (int i = 0; i < N; i++) {
-            graph[i] = new newNode();
-            graph[i].index = i;
+            friends[i] = new Friend();
+            friends[i].relationship = new ArrayList<>();
+            friends[i].num = i;
+            friends[i].isVisited = false;
         }
 
+        // 친구관계 입력 받기
         for (int i = 0; i < M; i++) {
             st = new StringTokenizer(br.readLine());
-            int u = Integer.parseInt(st.nextToken());
-            int v = Integer.parseInt(st.nextToken());
-
-            graph[u].friends.add(graph[v]);
-            graph[v].friends.add(graph[u]);
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            friends[a].relationship.add(b);
+            friends[b].relationship.add(a);
         }
 
-        // 모든 노드에서 DFS를 시작하여 탐색
-        boolean result = false;
+        // 돌면서 dfs
         for (int i = 0; i < N; i++) {
-            result = dfs(i, 0);
-            if (result) {
-                break;  // 이미 조건을 만족하는 경우 더 이상 확인할 필요 없음
-            }
+            visited = new boolean[N];
+            dfs(i, 0, visited);
+            // 이미 충족
+            if (result) break;
         }
 
         System.out.println(result ? 1 : 0);
+
     }
 
-    private static boolean dfs(int current, int depth) {
+    private static void dfs(int idx, int depth, boolean[] visited) {
+        // 기저
         if (depth == 4) {
-            return true;  // A, B, C, D, E와 같은 친구 관계가 만족하는 경우
+            result = true; // 가능합니다.
+//            // 출력
+//            for (int i = 0; i < N; i++) {
+//                System.out.print(visited[i] ? 1 : 0);
+//
+//            }
+//            System.out.println();
+            return;
         }
 
-        graph[current].visited = true;
-        for (newNode friend : graph[current].friends) {
-            if (!friend.visited) {
-                if (dfs(friend.index, depth + 1)) {
-                    return true;
-                }
+        // 유도
+        // idx의 친구를 돌았을때, 아직 방문하지 않았다고 판별이 된다면, dfs 실행 -> Depth + 1
+        friends[idx].isVisited = true;
+        for (Integer friend : friends[idx].relationship) {
+            if (!friends[friend].isVisited) {
+//                visited[friend] = true;
+                dfs(friend, depth + 1, visited);
+//                visited[friend] = false;
             }
         }
-
-        graph[current].visited = false;
-        return false;
+        friends[idx].isVisited = false;
     }
 }

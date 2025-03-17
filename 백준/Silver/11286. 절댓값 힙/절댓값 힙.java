2382e689b6ccfaff76e3870f14a872650431ef45
Yerim <101817171+SunYerim@users.noreply.github.com>
2025-03-17 16:08:52 +0900

    [Silver I] Title: 절댓값 힙, Time: 264 ms, Memory: 24964 KB -BaekjoonHub

--- 백준/Silver/11286. 절댓값 힙/절댓값 힙.java
+++ 백준/Silver/11286. 절댓값 힙/절댓값 힙.java
@@ -1,59 +1,50 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.Comparator;
 import java.util.PriorityQueue;
 
 public class Main {
+
     static int N;
-    static int[] arr;
-    static PriorityQueue<Node> node;
+    static PriorityQueue<Node> pq;
+    static StringBuilder sb = new StringBuilder();
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
         N = Integer.parseInt(br.readLine());
-        arr = new int[N];
-        node = new PriorityQueue<Node>(new Comparator<Node>() {
-            @Override
-            public int compare(Node o1, Node o2) {
-                if (o1.absNum == o2.absNum) {
-                    // 가장 작은 수 출력
-                    return o1.num - o2.num;
-                }
-                return o1.absNum - o2.absNum;
-            }
-        });
+        pq = new PriorityQueue<>();
 
-        int index = 0;
-        // 힙큐 사용
         for (int i = 0; i < N; i++) {
-            // 숫자를 넣으면서.
             int num = Integer.parseInt(br.readLine());
-            // 넣으려는 숫자가 0이면
-            if (num == 0) {
-                // 큐가 비어있지 않을 때만 poll하고, 비어있으면 0을 저장
-                if (!node.isEmpty()) {
-                    arr[index++] = node.poll().num;
-                } else {
-                    arr[index++] = 0;
-                }
+            if (num != 0) {
+                pq.add(new Node(num, Math.abs(num)));
             } else {
-                node.offer(new Node(num, Math.abs(num)));
+                if (pq.isEmpty()) sb.append(0).append("\n");
+                else {
+                    Node curr = pq.poll();
+                    sb.append(curr.num).append("\n");
+                }
             }
-
         }
 
-        for (int i = 0; i < index; i++) {
-            System.out.println(arr[i]);
-        }
+        System.out.print(sb.toString());
     }
 
-    static class Node {
-        int num;
-        int absNum;
+    // 객체 생성
+    static class Node implements Comparable<Node>{
+        int num, absNum;
         public Node(int num, int absNum) {
-            this.num = num;
-            this.absNum = absNum;
+            this.num = num; // 실제값
+            this.absNum = absNum; // 절댓값
+        }
+
+        @Override
+        public int compareTo(Node o) {
+            if (this.absNum == o.absNum) {
+                return this.num - o.num;
+            }
+            return this.absNum - o.absNum;
         }
     }
+
 }

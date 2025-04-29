6e93ba1f36bac37c236e8737ad9d9997ed3c374d
Yerim <101817171+SunYerim@users.noreply.github.com>
2025-04-29 16:16:32 +0900

    [level 3] Title: 카테고리 별 도서 판매량 집계하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/3/144855. 카테고리 별 도서 판매량 집계하기/카테고리 별 도서 판매량 집계하기.sql
@@ -0,0 +1,2 @@
+-- 코드를 입력하세요
+SELECT b.category as CATEGORY, sum(bs.sales) as TOTAL_SALES from book_sales bs left join book b on bs.book_id = b.book_id where date_format(bs.sales_date, '%Y-%m') = '2022-01' group by b.category order by category;

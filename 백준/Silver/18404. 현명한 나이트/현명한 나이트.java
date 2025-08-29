5e078eca2ffe8bcd1621b6fc225d1d67d93306be
Yerim <101817171+SunYerim@users.noreply.github.com>
2025-08-29 22:02:19 +0900

    [Silver I] Title: 현명한 나이트, Time: 276 ms, Memory: 32340 KB -BaekjoonHub

---
+++ 백준/Silver/18404. 현명한 나이트/현명한 나이트.java
@@ -0,0 +1,63 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static int N, M, X, Y;
+    static int[] dx = {-2, -2, -1, -1, 1, 1, 2, 2};
+    static int[] dy = {-1, 1, -2, 2, -2, 2, -1, 1};
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        st = new StringTokenizer(br.readLine());
+        X = Integer.parseInt(st.nextToken());
+        Y = Integer.parseInt(st.nextToken());
+
+        int[][] dist = new int[N + 1][N + 1];
+        for (int i = 0; i <= N; i++) {
+            Arrays.fill(dist[i], -1);
+        }
+
+        Queue<int[]> q = new LinkedList<>();
+        q.add(new int[]{X, Y});
+        dist[X][Y] = 0;
+        while (!q.isEmpty()) {
+            int[] cur = q.poll();
+            int x = cur[0];
+            int y = cur[1];
+
+            for (int i = 0; i < 8; i++) {
+                int nx = cur[0] + dx[i];
+                int ny = cur[1] + dy[i];
+
+                if (nx >= 1 && ny >= 1 && nx <= N && ny <= N && dist[nx][ny] == -1) {
+                    dist[nx][ny] = dist[x][y] + 1;
+                    q.add(new int[]{nx, ny});
+                }
+            }
+        }
+
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            sb.append(dist[a][b]).append(" ");
+        }
+
+        System.out.print(sb.toString());
+
+    }
+
+
+}

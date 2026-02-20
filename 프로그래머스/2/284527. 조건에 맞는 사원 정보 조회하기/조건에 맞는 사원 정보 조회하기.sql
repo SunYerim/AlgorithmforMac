91b608aa513541fad93ac3c089b65e93992ba8fb
Yerim <101817171+SunYerim@users.noreply.github.com>
2026-02-20 18:32:42 +0900

    [level 2] Title: 조건에 맞는 사원 정보 조회하기, Time: , Memory: undefined -BaekjoonHub

---
+++ 프로그래머스/2/284527. 조건에 맞는 사원 정보 조회하기/조건에 맞는 사원 정보 조회하기.sql
@@ -0,0 +1,21 @@
+-- 코드를 작성해주세요
+SELECT
+    SUM(HG.SCORE) AS SCORE,
+    HG.EMP_NO,
+    HE.EMP_NAME,
+    HE.POSITION,
+    HE.EMAIL
+FROM
+    HR_GRADE AS HG
+JOIN
+    HR_EMPLOYEES AS HE
+ON
+    HG.EMP_NO = HE.EMP_NO
+GROUP BY
+    HG.EMP_NO
+ORDER BY
+    SCORE DESC
+LIMIT 1;
+    
+    
+    

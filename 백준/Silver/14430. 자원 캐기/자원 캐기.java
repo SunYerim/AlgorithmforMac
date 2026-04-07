4d799f166fcb1a2689afcc6e4f60425ccb757842
Yerim <101817171+SunYerim@users.noreply.github.com>
2026-04-07 14:10:14 +0900

    [Silver II] Title: 자원 캐기, Time: 204 ms, Memory: 22672 KB -BaekjoonHub

---
+++ 백준/Silver/14430. 자원 캐기/자원 캐기.java
@@ -0,0 +1,35 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        int[][] board = new int[N][M];
+
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < M; j++) {
+                board[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        int[][] dp = new int[N + 1][M + 1];
+
+        for (int i = 1; i <= N; i++) {
+            for (int j = 1; j <= M; j++) {
+                dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]) + board[i - 1][j - 1];
+            }
+        }
+
+        System.out.println(dp[N][M]);
+
+    }
+
+}

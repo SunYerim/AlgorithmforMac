187243e824ab3e2c3a8d54dbff02ff20e7687aa0
Yerim <101817171+SunYerim@users.noreply.github.com>
2026-04-07 13:18:56 +0900

    [level 3] Title: 오랜 기간 보호한 동물(1), Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/59044. 오랜 기간 보호한 동물（1）/오랜 기간 보호한 동물（1）.sql
+++ 프로그래머스/3/59044. 오랜 기간 보호한 동물（1）/오랜 기간 보호한 동물（1）.sql
@@ -1,2 +1,15 @@
 -- 코드를 입력하세요
-SELECT ai.name as NAME, ai.datetime as DATETIME from animal_ins ai left outer join animal_outs ao on ai.animal_id = ao.animal_id where ai.name is not null and ao.name is null order by DATETIME limit 3;
+SELECT
+    I.NAME,
+    I.DATETIME
+FROM
+    ANIMAL_INS AS I
+LEFT JOIN
+    ANIMAL_OUTS AS O
+ON
+    I.ANIMAL_ID = O.ANIMAL_ID
+WHERE
+    O.ANIMAL_ID IS NULL
+ORDER BY
+    I.DATETIME ASC    
+LIMIT 3;
